e9b7ebc7e7409cc6c28b967d851f275903cd75d8
Nekuin <nekuin@gmail.com>
2024-01-03 16:24:16 +0200

    use tokyonight theme

--- init.lua
+++ init.lua
@@ -191,11 +191,12 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'tokyonight-moon'
     end,
+    opts = {}
   },
 
   {
@@ -205,7 +206,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight-moon',
         component_separators = '|',
         section_separators = '',
       },
